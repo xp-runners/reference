a575443cec854c0831ad27ee8a0fe7bcb08e6259
Timm Friebe <thekid@thekid.de>
2016-01-02 18:51:17 +0100

    Allow commands to be loaded from plugin development checkouts directly

--- src/commands/Plugin.cs
+++ src/commands/Plugin.cs
@@ -13,37 +13,54 @@ public class Plugin : Command
 
     private string entry;
     private string module;
-    private string composer;
+    private IEnumerable<string> modules;
     private static DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(Composer), new DataContractJsonSerializerSettings {
         UseSimpleDictionaryFormat = true
     });
 
     public Plugin(string name)
     {
-        foreach (var dir in ComposerLocations().Where(Directory.Exists))
+        foreach (var dir in ComposerLocations())
         {
-            var bin = Directory.GetFiles(Paths.Compose(dir, "bin"), "xp.*." + name).FirstOrDefault();
-            if (null == bin) continue;
+            if (FindCommand(dir, name))
+            {
+                modules = DependenciesAndSelf(dir, module, new HashSet<string>());
+                return;
+            }
+        }
 
-            var file = Path.GetFileName(bin).Split('.');
-
-            composer = dir;
-            module = file[VENDOR] + "/" + file[NAME];
-            entry = string.Format(
-                "xp.{0}.{1}Runner",
-                file[NAME],
-                file.Length > COMMAND ? file[COMMAND].UpperCaseFirst() : string.Empty
-            );
+        if (FindCommand(".", name)) 
+        {
+            modules = new string[] { "." };
             return;
         }
+
         throw new NotImplementedException(name);
     }
 
-    /// <summary>Returns the given module and a unique list of dependencies of a given module</summary>
-    private IEnumerable<string> DependenciesAndSelf(string module, HashSet<string> loaded)
+    /// <summary>Finds command by name in a given directory</summary>
+    private bool FindCommand(string dir, string name)
     {
-        var path = Paths.Compose(composer, module.Replace('/', Path.DirectorySeparatorChar));
+        if (!Directory.Exists(dir)) return false;
 
+        var bin = Directory.GetFiles(Paths.Compose(dir, "bin"), "xp.*." + name).FirstOrDefault();
+        if (null == bin) return false;
+
+        var file = Path.GetFileName(bin).Split('.');
+
+        module = file[VENDOR] + "/" + file[NAME];
+        entry = string.Format(
+            "xp.{0}.{1}Runner",
+            file[NAME],
+            file.Length > COMMAND ? file[COMMAND].UpperCaseFirst() : string.Empty
+        );
+        return true;
+    }
+
+    /// <summary>Returns the given module and a unique list of dependencies of a given module</summary>
+    private IEnumerable<string> DependenciesAndSelf(string dir, string module, HashSet<string> loaded)
+    {
+        var path = Paths.Compose(dir, module.Replace('/', Path.DirectorySeparatorChar));
         yield return path;
         loaded.Add(module);
 
@@ -54,7 +71,7 @@ public class Plugin : Command
             {
                 if (loaded.Contains(require.Key)) continue;
 
-                foreach (var transitive in DependenciesAndSelf(require.Key, loaded))
+                foreach (var transitive in DependenciesAndSelf(dir, require.Key, loaded))
                 {
                     yield return transitive;
                 }
@@ -65,7 +82,7 @@ public class Plugin : Command
     /// <summary>Additional modules to load. Overwrite in subclasses if necessary!</summary>
     protected override IEnumerable<string> ModulesFor(CommandLine cmd)
     {
-        return DependenciesAndSelf(module, new HashSet<string>());
+        return modules;
     }
 
     /// <summary>Command line arguments.</summary>
