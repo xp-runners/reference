85ee835e21a6727794cbfcf169fb8e6aaf6fbeb2
Timm Friebe <thekid@thekid.de>
2016-01-02 12:42:51 +0100

    Pass extensions and other ini settings defined in xp.ini

--- src/Command.cs
+++ src/Command.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 
 public abstract class Command
 {
-
     /// <summary>Main script, e.g. "class-main.php". Overwrite in subclasses if necessary!</summary>
     protected virtual string MainFor(CommandLine cmd)
     {
@@ -24,22 +23,35 @@ public abstract class Command
         return cmd.Arguments;
     }
 
+    /// <summary>Format ini settings for use in command line</summary>
+    private IEnumerable<string> IniSettings(IEnumerable<KeyValuePair<string, IEnumerable<string>>> arguments)
+    {
+        return arguments.SelectMany(pair => pair.Value.Select(setting => string.Format("-d {0}={1}", pair.Key, setting)));
+    }
+
     /// <summary>Entry point</summary>
     public int Execute(CommandLine cmd, ConfigSource configuration)
     {
         var proc = new Process();
         var runtime = configuration.GetRuntime();
+        var ini = new Dictionary<string, IEnumerable<string>>()
+        {
+            { "encoding", new string[] { "utf-7" } },
+            { "magic_quotes_gpc", new string[] { "0" } },
+            { "date.timezone", new string[] { TimeZoneInfo.Local.Olson() ?? "UTC" } },
+            { "extension", configuration.GetExtensions(runtime) }
+        };
 
         proc.StartInfo.RedirectStandardOutput = false;
         proc.StartInfo.RedirectStandardError = false;
         proc.StartInfo.UseShellExecute = false;
         proc.StartInfo.FileName = configuration.GetExecutable(runtime) ?? "php";
         proc.StartInfo.Arguments = string.Format(
-            "-C -q -d include_path=\".{0}{1}{0}{0}.{0}{2}\" -d encoding=utf-7 -d date.timezone={3} -d magic_quotes_gpc=0 {4} {5}",
+            "-C -q -d include_path=\".{0}{1}{0}{0}.{0}{2}\" {3} {4} {5}",
             Paths.Separator,
             string.Join(Paths.Separator, configuration.GetUse().Concat(cmd.Options["modules"]).Concat(ModulesFor(cmd))),
             string.Join(Paths.Separator, cmd.Options["classpath"]),
-            TimeZoneInfo.Local.Olson() ?? "UTC",
+            string.Join(" ", IniSettings(configuration.GetArgs(runtime).Concat(ini))),
             MainFor(cmd),
             string.Join(" ", ArgumentsFor(cmd).Select(Strings.AsArgument))
         );
