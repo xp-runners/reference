2b10ca655827411c702c88d03945cc8ea81af164
Timm Friebe <thekid@thekid.de>
2016-01-03 14:52:58 +0100

    Add vendor information into executable

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("XP-Framework Team")]
+[assembly: AssemblyProduct("XP Runners")]
+[assembly: AssemblyTitle("XP Runner")]
+[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2016")]
+[assembly: AssemblyVersion("7.0.0.1451")]
+[assembly: TargetFramework(".NETFramework,Version=v5.0")]
