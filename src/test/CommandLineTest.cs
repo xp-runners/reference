f484630d5a70f745039bc4b1f204e45967cfce37
Timm Friebe <thekid@thekid.de>
2016-01-03 16:28:12 +0100

    Add tests for classpath, modules and builtin commands

--- src/test/CommandLineTest.cs
+++ src/test/CommandLineTest.cs
@@ -12,9 +12,103 @@ namespace Xp.Runners.Test
         }
 
         [Fact]
-        public void supply_command()
+        public void supply_run_command()
         {
             Assert.IsType<Run>(new CommandLine(new string[] { "run", "Test" }).Command);
+        }
+
+        [Fact]
+        public void omit_run_command_but_pass_type()
+        {
+            Assert.IsType<Run>(new CommandLine(new string[] { "Test" }).Command);
+        }
+
+        [Theory]
+        [InlineData("-?")]
+        [InlineData("help")]
+        public void help(string arg)
+        {
+            Assert.IsType<Help>(new CommandLine(new string[] { arg }).Command);
+        }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("version")]
+        public void version(string arg)
+        {
+            Assert.IsType<Version>(new CommandLine(new string[] { arg }).Command);
+        }
+
+        [Theory]
+        [InlineData("-e")]
+        [InlineData("eval")]
+        public void eval(string arg)
+        {
+            Assert.IsType<Eval>(new CommandLine(new string[] { arg }).Command);
+        }
+
+        [Theory]
+        [InlineData("-w")]
+        [InlineData("write")]
+        public void write(string arg)
+        {
+            Assert.IsType<Write>(new CommandLine(new string[] { arg }).Command);
+        }
+
+        [Theory]
+        [InlineData("-d")]
+        [InlineData("dump")]
+        public void dump(string arg)
+        {
+            Assert.IsType<Dump>(new CommandLine(new string[] { arg }).Command);
+        }
+
+        [Fact]
+        public void classpath_initially_empty()
+        {
+            Assert.Equal(new string[] { }, new CommandLine(new string[] { }).Options["classpath"].ToArray());
+        }
+
+        [Fact]
+        public void one_classpath_entry()
+        {
+            Assert.Equal(
+                new string[] { "src/main/php" },
+                new CommandLine(new string[] { "-cp", "src/main/php" }).Options["classpath"].ToArray()
+            );
+        }
+
+        [Fact]
+        public void multiple_classpath_entries()
+        {
+            Assert.Equal(
+                new string[] { "src/main/php", "src/test/php" },
+                new CommandLine(new string[] { "-cp", "src/main/php", "-cp", "src/test/php" }).Options["classpath"].ToArray()
+            );
+        }
+
+        [Fact]
+        public void modules_initially_empty()
+        {
+            Assert.Equal(new string[] { }, new CommandLine(new string[] { }).Options["modules"].ToArray());
+        }
+
+        [Fact]
+        public void one_modules_entry()
+        {
+            Assert.Equal(
+                new string[] { "test" },
+                new CommandLine(new string[] { "-m", "test" }).Options["modules"].ToArray()
+            );
+        }
+
+        [Fact]
+        public void multiple_modules_entries()
+        {
+            Assert.Equal(
+                new string[] { "test", "data" },
+                new CommandLine(new string[] { "-m", "test", "-m", "data" }).Options["modules"].ToArray()
+            );
         }
     }
 }
