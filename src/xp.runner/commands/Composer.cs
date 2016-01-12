6050f8992dfa51ca92d3bb7e8014b73483dfd910
Timm Friebe <thekid@thekid.de>
2016-01-12 23:36:45 +0100

    Remove DataContract, this is done manually now

--- src/xp.runner/commands/Composer.cs
+++ src/xp.runner/commands/Composer.cs
@@ -1,17 +1,13 @@
-using System.Runtime.Serialization;
 using System.Collections.Generic;
 using Xp.Runners;
 
 namespace Xp.Runners.Commands
 {
     /// <summary>A stripped-down representation of a composer.json file</summary>
-    [DataContract]
     public class Composer
     {
-        [DataMember(Name = "name")]
         public string Name { get; set; }
 
-        [DataMember(Name = "require")]
         public Dictionary<string, string> Require { get; set; }
     }
 }
