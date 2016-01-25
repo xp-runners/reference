0bb271e80d905692fd196e942d7da39441e6afb7
Timm Friebe <thekid@thekid.de>
2016-01-25 18:53:38 +0100

    Detect whether process has exited after less than two seconds at start

--- src/xp.runner/exec/Supervise.cs
+++ src/xp.runner/exec/Supervise.cs
@@ -9,6 +9,7 @@ namespace Xp.Runners.Exec
     public class Supervise : ExecutionModel
     {
         const int WAIT_BEFORE_RESPAWN = 1;
+        const int WAIT_FOR_STARTUP = 2;
 
         /// <summary>Execute the process and return its exitcode</summary>
         public override int Execute(Process proc, Encoding encoding)
@@ -16,13 +17,23 @@ namespace Xp.Runners.Exec
             int code = -1;
             do
             {
+                var start = DateTime.Now;
+                code = Run(proc, encoding);
+                var elapsed = DateTime.Now - start;
+
                 if (code > 0)
                 {
-                    Console.WriteLine("*** Process exited with exitcode {0}, respawning...", code);
-                    Thread.Sleep(WAIT_BEFORE_RESPAWN * 1000);
+                    if (elapsed.Seconds < WAIT_FOR_STARTUP)
+                    {
+                        Console.WriteLine("*** Process exited too quickly, aborting", code);
+                        return code;
+                    }
+                    else
+                    {
+                        Console.WriteLine("*** Process exited with exitcode {0}, respawning...", code);
+                        Thread.Sleep(WAIT_BEFORE_RESPAWN * 1000);
+                    }
                 }
-
-                code = Run(proc, encoding);
             } while (code > 0);
 
             return code;
