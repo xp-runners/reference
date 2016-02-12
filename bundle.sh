5002faedc2dff96e2b8820b21cbe911e08f61d46
Timm Friebe <thekid@thekid.de>
2016-02-12 16:51:24 +0100

    Determine tempdir via TMPDIR ?? TEMP ?? TMP ?? /tmp

--- bundle.sh
+++ bundle.sh
@@ -8,13 +8,8 @@ if [ -z ${TRAVIS_TAG-} ]; then
   exit 1
 fi
 
-if [ -z "$TMPDIR" ]; then
-    TEMPDIR="$TMPDIR"
-else
-    TEMPDIR="/tmp"
-fi
-
-BUILD=$(mktemp -d "$TEMPDIR/tmp.XXXXXXXXXX")
+temp=${TMPDIR-${TEMP-${TMP-/tmp}}}
+BUILD=$(mktemp -d "$temp/tmp.XXXXXXXXXX")
 SOURCES="bootstrap.php class-main.php class-path.php scan-path.php web-main.php xar-support.php entry.php"
 TARGETS="class-main.php web-main.php"
 
