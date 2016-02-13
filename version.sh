228dd305df5705268fe7b6a38d7cfd156df9f24b
Timm Friebe <thekid@thekid.de>
2016-02-13 20:02:21 +0100

    Ensure version numer is always in sync w/ tag

---
+++ version.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+set -u 
+
+if [ -z ${TRAVIS_TAG-} ]; then
+  RELEASE=$(grep '##' ChangeLog.md | grep -v ???? | head -1 | cut -d ' ' -f 2)
+  BUILD=${TRAVIS_BUILD_NUMBER}
+else
+  RELEASE=${TRAVIS_TAG#v*}
+  BUILD=${TRAVIS_BUILD_NUMBER}
+fi
+
+rm -f src/xp.runner/AssemblyInfo.cs.patched
+grep -v AssemblyVersion src/xp.runner/AssemblyInfo.cs >> src/xp.runner/AssemblyInfo.cs.patched
+echo '[assembly: AssemblyVersion("'$RELEASE'.'$BUILD'")]' >> src/xp.runner/AssemblyInfo.cs.patched
+mv src/xp.runner/AssemblyInfo.cs.patched src/xp.runner/AssemblyInfo.cs
