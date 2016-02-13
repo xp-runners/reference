f0824b09d31dd4876576cd64697380f1e9e02a2d
Timm Friebe <thekid@thekid.de>
2016-02-13 16:29:11 +0100

    No longer create *mkbundle'd* executables
    These are not portable and link to libraries which may not be
    in place in this versions (but, on the other hand also cannot
    bundle everything due to licensing / type mismatch problems).
    
    See https://github.com/xp-runners/reference/issues/36#issuecomment-183530767

--- bundle.sh
+++ bundle.sh
@@ -10,7 +10,6 @@ BUILD=$(mktemp -d "$temp/tmp.XXXXXXXXXX")
 SOURCES="bootstrap.php class-main.php class-path.php scan-path.php web-main.php xar-support.php entry.php"
 TARGETS="class-main.php web-main.php"
 EXE=$(pwd)/xp.exe
-BIN=$(pwd)/xp
 
 # Fetch
 for src in $SOURCES ; do
@@ -23,9 +22,6 @@ curl -sSL https://raw.githubusercontent.com/xp-runners/main/master/inline.pl > $
 for target in $TARGETS ; do
   cat $BUILD/$target | perl $BUILD/inline.pl $BUILD > $target
 done
-
-# Standalone XP
-mkbundle -o $BIN $EXE System.Core System.Runtime.Serialization Mono.Posix -z
 
 # Done
 rm -rf $BUILD
