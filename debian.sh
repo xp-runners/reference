ea5a676dc5d4b6dbccf070bd06e13281c0cdd3a0
Timm Friebe <thekid@thekid.de>
2016-07-15 19:20:50 +0200

    Fix dependencies to work on Ubuntu Xenial and Debian Jessie
    See #59

--- debian.sh
+++ debian.sh
@@ -40,7 +40,7 @@ cat <<-EOF > control
 	Maintainer: XP Team <xp-runners@xp-framework.net>
 	Architecture: all
 	Version: ${VERSION}-1
-	Depends: php5-cli, libmono-corlib4.5-cil, libmono-system-core4.0-cil
+	Depends: php5-cli | php7.0-cli, libmono-corlib4.5-cil, libmono-system-core4.0-cil, libmono-system-runtime-serialization4.0-cil
 	Provides: xp-runners
 	Description: XP Runners
 EOF
