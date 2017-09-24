5cbbd7672818f62547ce0d5c83e312dd2a6c73d3
Timm Friebe <thekid@thekid.de>
2017-09-24 14:17:27 +0200

    Make compatible with PHP 7.1

--- debian.sh
+++ debian.sh
@@ -47,7 +47,7 @@ cat <<-EOF > control
 	Maintainer: XP Team <xp-runners@xp-framework.net>
 	Architecture: all
 	Version: ${VERSION}-1
-	Depends: php5-cli | php7.0-cli, libmono-corlib4.5-cil, libmono-system-core4.0-cil, libmono-system-runtime-serialization4.0-cil
+	Depends: php7.1-cli | php7.0-cli | php5-cli, libmono-corlib4.5-cil, libmono-system-core4.0-cil, libmono-system-runtime-serialization4.0-cil
 	Provides: xp-runners
 	Description: XP Runners
 EOF
