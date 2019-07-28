ebeca18b8af1d823a962f2cbfbb07f1c50f6acfd
Timm Friebe <thekid@thekid.de>
2019-07-28 12:27:55 +0200

    Publish master as "snapshot-{$COMMIT_SHA}"
    Bintray does not allow overwriting releases, and the workaround of
    deleting package versions and republishing them no longer works
    
    "Published artifacts may be redeployed within 365 days"
    See https://www.jfrog.com/confluence/display/BT/Bintray+REST+API

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -z ${TRAVIS_TAG-} ]; then
-  VERSION=master
   VCS_TAG=${TRAVIS_COMMIT-$(git log --pretty=format:'%h' --abbrev-commit --date=short -1)}
-  echo "Publishing master"
+  VERSION="snapshot-$VCS_TAG"
+  echo "Publishing master ($VCS_TAG)"
 else
   VERSION=${TRAVIS_TAG#v*}
   VCS_TAG=$TRAVIS_TAG
