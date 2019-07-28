8d78fe4939743142dc75a0574d2d3d7db4e22863
Timm Friebe <thekid@thekid.de>
2019-07-28 11:32:40 +0200

    Extend copyright year

--- src/xp.runner/AssemblyInfo.cs
+++ src/xp.runner/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("XP-Framework Team")]
 [assembly: AssemblyProduct("XP Runners")]
 [assembly: AssemblyTitle("XP Runner")]
-[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2018")]
+[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2019")]
 [assembly: AssemblyVersion("8.1.5.1959")]
 [assembly: TargetFramework(".NETFramework,Version=v5.0")]
