75c5a1ae938df1bc07e666c74be516c0c7ff5d0d
Timm Friebe <thekid@thekid.de>
2021-04-17 13:29:28 +0200

    Prepare 8.4.0

--- src/xp.runner/AssemblyInfo.cs
+++ src/xp.runner/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.Versioning;
 [assembly: AssemblyProduct("XP Runners")]
 [assembly: AssemblyTitle("XP Runner")]
 [assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2021")]
-[assembly: AssemblyVersion("8.3.1.1244")]
+[assembly: AssemblyVersion("8.4.0.1329")]
 [assembly: TargetFramework(".NETFramework,Version=v5.0")]
