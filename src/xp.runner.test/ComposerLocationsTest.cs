8c0a18d4080c5661b1d6b73e67d82a662377c575
Timm Friebe <thekid@thekid.de>
2021-12-06 13:27:21 +0100

    Use new ModifiedEnvironment test utility

--- src/xp.runner.test/ComposerLocationsTest.cs
+++ src/xp.runner.test/ComposerLocationsTest.cs
@@ -1,32 +1,13 @@
 using Xunit;
 using Xunit.Extensions;
 using System;
-using System.Collections;
 using System.Linq;
 using Xp.Runners.IO;
 
 namespace Xp.Runners.Test
 {
-    public class ComposerLocationsTest : IDisposable
+    public class ComposerLocationsTest
     {
-        private IDictionary environment;
-
-        public ComposerLocationsTest()
-        {
-            environment = Environment.GetEnvironmentVariables();
-            foreach (string name in environment.Keys)
-            {
-                if (name.StartsWith("XDG_")) Environment.SetEnvironmentVariable(name, null);
-            }
-        }
-
-        public void Dispose()
-        {
-            foreach (string name in environment.Keys)
-            {
-                Environment.SetEnvironmentVariable(name, environment[name] as string);
-            }
-        }
 
         [Fact]
         public void uses_appdata_on_windows()
@@ -70,41 +51,46 @@ namespace Xp.Runners.Test
         public void uses_xdg_config_home_if_available_and_composerdotdir_does_not_exist()
         {
             var config = Paths.Compose(Paths.Home(), ".configuration");
-            Environment.SetEnvironmentVariable("XDG_CONFIG_HOME", config);
-            Assert.Equal(
-                new string[] {
-                    Paths.Compose(".", ComposerLocations.VENDOR),
-                    Paths.Compose(config, "composer", ComposerLocations.VENDOR),
-                },
-                ComposerLocations.For(PlatformID.Unix, (dir) => false).ToArray()
-            );
+            using (new ModifiedEnvironment().RemoveAny("XDG_").With("XDG_CONFIG_HOME", config))
+            {
+                Assert.Equal(
+                    new string[] {
+                        Paths.Compose(".", ComposerLocations.VENDOR),
+                        Paths.Compose(config, "composer", ComposerLocations.VENDOR),
+                    },
+                    ComposerLocations.For(PlatformID.Unix, (dir) => false).ToArray()
+                );
+            }
         }
 
         [Fact]
         public void uses_xdg_default_config_home_if_composerdotdir_does_not_exist()
         {
-            Environment.SetEnvironmentVariable("XDG_RUNTIME_DIR", "/run/user/1000");
-            Assert.Equal(
-                new string[] {
-                    Paths.Compose(".", ComposerLocations.VENDOR),
-                    Paths.Compose(Paths.Home(), ".config", "composer", ComposerLocations.VENDOR),
-                },
-                ComposerLocations.For(PlatformID.Unix, (dir) => false).ToArray()
-            );
+            using (new ModifiedEnvironment().RemoveAny("XDG_").With("XDG_RUNTIME_DIR", "/run/user/1000"))
+            {
+                Assert.Equal(
+                    new string[] {
+                        Paths.Compose(".", ComposerLocations.VENDOR),
+                        Paths.Compose(Paths.Home(), ".config", "composer", ComposerLocations.VENDOR),
+                    },
+                    ComposerLocations.For(PlatformID.Unix, (dir) => false).ToArray()
+                );
+            }
         }
 
         [Fact]
         public void does_not_use_xdg_config_home_if_composerdotdir_exists()
         {
-            Environment.SetEnvironmentVariable("XDG_RUNTIME_DIR", "/run/user/1000");
-            Assert.Equal(
-                new string[] {
-                    Paths.Compose(".", ComposerLocations.VENDOR),
-                    Paths.Compose(Paths.Home(), ".composer", ComposerLocations.VENDOR),
-                },
-                ComposerLocations.For(PlatformID.Unix, (dir) => true).ToArray()
-            );
+            using (new ModifiedEnvironment().RemoveAny("XDG_").With("XDG_RUNTIME_DIR", "/run/user/1000"))
+            {
+                Assert.Equal(
+                    new string[] {
+                        Paths.Compose(".", ComposerLocations.VENDOR),
+                        Paths.Compose(Paths.Home(), ".composer", ComposerLocations.VENDOR),
+                    },
+                    ComposerLocations.For(PlatformID.Unix, (dir) => true).ToArray()
+                );
+            }
         }
-
     }
 }
