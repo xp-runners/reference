b50d4e6cdcaf9588ebf06076a1465342f387239a
Timm Friebe <thekid@thekid.de>
2025-02-22 10:25:19 +0100

    Bump copyright year

--- src/xp.runner/AssemblyInfo.cs
+++ src/xp.runner/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("XP-Framework Team")]
 [assembly: AssemblyProduct("XP Runners")]
 [assembly: AssemblyTitle("XP Runner")]
-[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2023")]
+[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2025")]
 [assembly: TargetFramework(".NETFramework,Version=v5.0")]
 [assembly: AssemblyVersion("1.2.3.1")]
