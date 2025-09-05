e51583ad8be0bbad295d66c0e74082c40026f7ee
Timm Friebe <thekid@thekid.de>
2025-09-05 23:30:41 +0200

    Publish artifacts instead of pushing to Balto
    See issue #90

---
+++ .github/workflows/downloads.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+set -u
+
+VERSION=${GITHUB_REF#refs/tags/v*}
+TARGET=$(pwd)/target
+SETUP=$TARGET/setup-${VERSION}.sh
+ENTRYPOINT=$TARGET/xp-run-${VERSION}.sh
+TAR=$TARGET/xp-runners_${VERSION}.tar.gz
+ZIP=$TARGET/xp-runners_${VERSION}.zip
+
+mkdir -p $TARGET
+rm -rf $TAR $ZIP
+
+# One-line installer
+cat setup.sh.in | sed -e "s/@VERSION@/$VERSION/g" > $SETUP
+
+# Shell entrypoint
+(cat xp-run.sh.in | sed -e "s/@VERSION@/$VERSION/g" ; cat class-main.php ) > $ENTRYPOINT
+
+# Tar.gz for Un*x
+tar cvfz $TAR xp.exe xar.exe tput.exe class-main.php web-main.php
+
+# Zipfile for Windows
+zip -j $ZIP xp.exe xar.exe tput.exe class-main.php web-main.php
+curl -i \
+  --header "Authorization: Bearer ${BALTO_REPO_TOKEN}" \
+  --form "download=@${ZIP}" \
+  --form "name=zip" \
+  --form "version=${VERSION}" \
+  --form "description=Zip file" \
+  --form "readme=<README.md" \
+  $PUBLISH
+echo
+
+# Done
+echo "Downloads"
+ls -al $SETUP $ENTRYPOINT $TAR $ZIP
