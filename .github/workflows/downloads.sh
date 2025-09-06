be0d45e36b5eb4794484d10e3a826f77cac3b35c
Timm Friebe <thekid@thekid.de>
2025-09-06 10:11:15 +0200

    Create .deb file and release it

--- .github/workflows/downloads.sh
+++ .github/workflows/downloads.sh
@@ -3,28 +3,72 @@
 set -e
 set -u
 
+ORIGIN=$(pwd)
+TARGET=$ORIGIN/$1
+BUILD=$(mktemp -d)
+
 VERSION=${GITHUB_REF#refs/tags/v*}
-TARGET=$(pwd)/$1
 SETUP=$TARGET/setup-${VERSION}.sh
 ENTRYPOINT=$TARGET/xp-run-${VERSION}.sh
 TAR=$TARGET/xp-runners_${VERSION}.tar.gz
 ZIP=$TARGET/xp-runners_${VERSION}.zip
+DEB=$TARGET/xp-runners_${VERSION}-1_all.deb
 
 mkdir -p $TARGET
-rm -rf $TAR $ZIP
+rm -rf $TAR $ZIP $DEB
 
-# One-line installer
+echo "=> One-line installer"
 cat setup.sh.in | sed -e "s/@VERSION@/$VERSION/g" > $SETUP
 
-# Shell entrypoint
+echo "=> Shell entrypoint"
 (cat xp-run.sh.in | sed -e "s/@VERSION@/$VERSION/g" ; cat class-main.php ) > $ENTRYPOINT
 
-# Tar.gz for Un*x
+echo "=> Tar.gz for Un*x"
 tar cvfz $TAR xp.exe xar.exe tput.exe class-main.php web-main.php
 
-# Zipfile for Windows
+echo "=> Zipfile for Windows"
 zip -j $ZIP xp.exe xar.exe tput.exe class-main.php web-main.php
 
+echo "=> Debian package"
+fakeroot=$(which fakeroot || which fakeroot-ng)
+
+# data.tar.xz
+mkdir -p $BUILD/usr/bin
+
+printf '#!/bin/sh\nexec /usr/bin/mono /usr/bin/xp.exe "$@"\n' > $BUILD/usr/bin/xp
+chmod 755 $BUILD/usr/bin/xp
+
+printf '#!/bin/sh\nexec /usr/bin/mono /usr/bin/xp.exe ar "$@"\n' > $BUILD/usr/bin/xar
+chmod 755 $BUILD/usr/bin/xar
+
+cp xp.exe $BUILD/usr/bin/xp.exe
+cp class-main.php web-main.php $BUILD/usr/bin
+
+cd $BUILD
+$fakeroot tar cfJ data.tar.xz usr/bin/*
+
+# control.tar.gz
+size=$(du -k usr/bin | cut -f 1)
+echo -n '' > conffiles
+cat <<-EOF > control
+  Package: xp-runners
+  Priority: extra
+  Section: devel
+  Installed-Size: ${size}
+  Maintainer: XP Team <xp-runners@xp-framework.net>
+  Architecture: all
+  Version: ${VERSION}-1
+  Depends: php8.4-cli | php8.3-cli | php8.2-cli | php8.1-cli | php8.0-cli | php7.4-cli, libmono-corlib4.5-cil, libmono-system-core4.0-cil, libmono-system-runtime-serialization4.0-cil
+  Provides: xp-runners
+  Description: XP Runners
+EOF
+$fakeroot tar cfz control.tar.gz ./conffiles ./control
+cat control
+
+echo '2.0' > debian-binary
+ar -q $DEB debian-binary control.tar.gz data.tar.xz
+cd $ORIGIN
+
 # Done
-echo "Downloads"
-ls -al $SETUP $ENTRYPOINT $TAR $ZIP
+echo "=> Downloads"
+ls -al $SETUP $ENTRYPOINT $TAR $ZIP $DEB
